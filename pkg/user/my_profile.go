d15b396cd6ebfb89ec7f1ea5862702404e3fa3b4
ecto0310 <ecto0310@gmail.com>
2022-03-20 03:30:03 +0900

    [add] get user profile

---
+++ pkg/user/my_profile.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Profile struct {
+	UserId                string    `json:"id"`
+	Username              string    `json:"username"`
+	SchoolGrade           int       `json:"school_grade"`
+	IconURL               string    `json:"icon_url"`
+	DiscordUserId         string    `json:"discord_userid"`
+	ActiveLimit           time.Time `json:"active_limit"`
+	ShortSelfIntroduction string    `json:"short_self_introduction"`
+}
+
+func (p Profile) validate() error {
+	return nil
+}
+
+type ResponseGetMyProfile struct {
+	Profile Profile `json:"profile"`
+	Error   string  `json:"error"`
+}
+
+type ResponseSetMyProfile struct {
+	Error string `json:"error"`
+}
+
+// Get my prodile
+// @Accept json
+// @Router /user/my [get]
+// @Success 200 {object} ResponseGetMyProfile
+func (c Context) GetMyProfile(e echo.Context) error {
+	userId, err := GetUserId(&e)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, ResponseGetMyProfile{Error: ""})
+	}
+	profile := Profile{UserId: userId}
+	err = c.DB.QueryRow("SELECT username, school_grade, icon_url, discord_userid, short_self_introduction FROM UserProfile WHERE user_id = UUID_TO_BIN(?)", userId).
+		Scan(&profile.Username, &profile.SchoolGrade, &profile.IconURL, &profile.DiscordUserId, &profile.ShortSelfIntroduction)
+	if err == sql.ErrNoRows {
+		return e.JSON(http.StatusBadRequest, ResponseGetMyProfile{Error: err.Error()})
+	} else if err != nil {
+		return e.JSON(http.StatusBadRequest, ResponseGetMyProfile{Error: err.Error()})
+	}
+	return e.JSON(http.StatusOK, ResponseGetMyProfile{Profile: profile})
+}
+
