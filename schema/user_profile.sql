2a1c6f6044eb30a741e3b3358e62bfcae9393d2d
ecto0310 <ecto0310@gmail.com>
2022-04-13 15:08:47 +0900

    [fix] default value

--- schema/user_profile.sql
+++ schema/user_profile.sql
@@ -6,8 +6,8 @@ CREATE TABLE UserProfile
     school_grade            INT          NOT NULL,
     icon_url                VARCHAR(255) NOT NULL,
     discord_userid          VARCHAR(255) NOT NULL DEFAULT '',
-    active_limit            DATE         NOT NULL DEFAULT (CURRENT_DATE),
-    short_self_introduction VARCHAR(255) NOT NULL,
+    active_limit            DATE         NOT NULL,
+    short_self_introduction VARCHAR(255) NOT NULL DEFAULT 'デジクリ入りました',
     self_introduction       VARCHAR(255) NOT NULL DEFAULT '',
     PRIMARY KEY (id),
     CONSTRAINT fk_user_id_user_profile FOREIGN KEY (user_id) REFERENCES User (id)
