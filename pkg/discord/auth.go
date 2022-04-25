1874f4d7a752055220a842ba68687c861a1a9f9d
ecto0310 <ecto0310@gmail.com>
2022-04-25 23:48:01 +0900

    [fix] redirect url

--- pkg/discord/auth.go
+++ pkg/discord/auth.go
@@ -13,5 +13,5 @@ import (
 // @Success 302 "redirect to oauth page"
 // @Header 302 {string}  Location
 func (c Context) OAuthURL(e echo.Context) error {
-	return e.Redirect(http.StatusFound, fmt.Sprintf("https://discord.com/api/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=code&scope=identify", env.DiscordClientID, env.DiscordRedirectURL))
+	return e.Redirect(http.StatusFound, fmt.Sprintf("https://discord.com/api/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=code&scope=identify", env.DiscordClientID, env.BackendRootURL+"/discord/oauth/callback"))
 }
