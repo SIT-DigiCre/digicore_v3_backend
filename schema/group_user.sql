ff4451e0ef6dd8ae5688f8fa00cd2ac1689df60b
ecto0310 <ecto0310@gmail.com>
2022-05-06 16:02:27 +0900

    [fix] db schema

--- schema/group_user.sql
+++ schema/group_user.sql
@@ -3,5 +3,7 @@ CREATE TABLE GroupUser
     id       BINARY(16) NOT NULL DEFAULT (UUID_TO_BIN(UUID())),
     user_id  BINARY(16) NOT NULL,
     group_id BINARY(16) NOT NULL,
+    UNIQUE uk_user_id_group_id (user_id, group_id),
+    CONSTRAINT fk_group_id_group_user FOREIGN KEY (group_id) REFERENCES `Group`(id),
     PRIMARY KEY (id)
 );
