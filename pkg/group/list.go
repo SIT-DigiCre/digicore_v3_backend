35ebca9abc54cac627643977e2ae269e4cbf040c
ecto0310 <ecto0310@gmail.com>
2022-05-09 00:45:16 +0900

    [add] error handling

--- pkg/group/list.go
+++ pkg/group/list.go
@@ -24,6 +24,9 @@ func (c Context) GroupList(e echo.Context) error {
 		return e.JSON(http.StatusBadRequest, ResponseList{Error: err.Error()})
 	}
 	rows, err := c.DB.Query("SELECT BIN_TO_UUID(Group.id), name, description, `join`, (CASE WHEN GroupUser.user_id IS NOT NULL THEN true ELSE false END) AS joined  FROM `Group` LEFT JOIN GroupUser ON Group.id = GroupUser.group_id AND GroupUser.user_id = UUID_TO_BIN(?)", userId)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, ResponseList{Error: "DBの読み込みに失敗しました"})
+	}
 	var groups []Group
 	for rows.Next() {
 		group := Group{}
