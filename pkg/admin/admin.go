15d13003e77970f8eb85c5438d5f5df929460683
rubisaku <rubisaku01@gmail.com>
2022-06-11 16:53:41 +0900

    [fix] db query

--- pkg/admin/admin.go
+++ pkg/admin/admin.go
@@ -27,8 +27,8 @@ func Middleware(db *sql.DB) echo.MiddlewareFunc {
 				return echo.NewHTTPError(http.StatusBadRequest)
 			}
 			count := 0
-			err = db.QueryRow("SELECT count(*) FROM groups_users LEFT JOIN users ON groups_users.user_id = users.id WHERE users.id = ? AND group_id = ?", id, env.AdminGroupID).Scan(&count)
-			if err != nil {
+			err = db.QueryRow("SELECT count(*) FROM groups_users LEFT JOIN users ON groups_users.user_id = users.id WHERE users.id = UUID_TO_BIN(?) AND group_id = UUID_TO_BIN(?)", id, env.AdminGroupID).Scan(&count)
+			if err != nil || count == 0 {
 				return echo.NewHTTPError(http.StatusUnauthorized)
 			}
 			return next(c)
