b36c69dcb69eb2d1f30d690fd9713b26cf190b4b
ecto0310 <ecto0310@gmail.com>
2022-06-11 22:52:38 +0900

    [add] cancel reservation

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -72,6 +72,7 @@ func addRouting(e *echo.Echo, db *sql.DB) {
 	event_group.GET("", event.GetEventsList)
 	event_group.GET("/:id", event.GetEventDetail)
 	event_group.POST("/:event_id/:id", event.Reservation)
+	event_group.DELETE("/:event_id/:id", event.CancelReservation)
 
 	s := e.Group("/storage")
 	s.Use(middleware.JWTWithConfig(config))
