ac169cca3b65f5e8f5f0c546bf2fcc1cb8f95c02
ecto0310 <ecto0310@gmail.com>
2022-06-25 11:52:36 +0900

    [fix] event detail response

--- pkg/event/detail.go
+++ pkg/event/detail.go
@@ -2,26 +2,33 @@ package event
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/user"
 	"github.com/labstack/echo/v4"
 )
 
 type ResponseEventDetail struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Reservated  bool   `json:"reservated"`
-	Detail []Detail `json:"detail"`
-	Error  string   `json:"error"`
+	Id               string             `json:"id"`
+	Name             string             `json:"name"`
+	Description      string             `json:"description"`
+	Reservated       bool               `json:"reservated"`
+	ReservationFrame []ReservationFrame `json:"reservation_frame"`
+	Error            string             `json:"error"`
 }
 
-type Detail struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Full        bool   `json:"full"`
-	Reservated  bool   `json:"reservated"`
+type ReservationFrame struct {
+	Id                    string    `json:"id"`
+	Name                  string    `json:"name"`
+	Description           string    `json:"description"`
+	StartDate             time.Time `json:"start_date"`
+	FinishDate            time.Time `json:"finish_date"`
+	ReservationStartDate  time.Time `json:"reservation_start_date"`
+	ReservationFinishDate time.Time `json:"reservation_finish_date"`
+	Capacity              int       `json:"capacity"`
+	FreeCapacity          int       `json:"free_capacity"`
+	Reservable            bool      `json:"reservable"`
+	Reservated            bool      `json:"reservated"`
 }
 
 // Get event detail
@@ -35,23 +42,29 @@ func (c Context) GetEventDetail(e echo.Context) error {
 		return e.JSON(http.StatusBadRequest, ResponseEventsList{Error: err.Error()})
 	}
 	id := e.Param("id")
-	rows, err := c.DB.Query("SELECT BIN_TO_UUID(event_reservations.id), event_reservations.name, event_reservations.Description, IF(event_reservations.reservation_limit <= count(event_reservation_users.id) ,true ,false) AS full, IF(SUM(event_reservation_users.user_id = UUID_TO_BIN(?)) ,true ,false) AS reservated  FROM event_reservations LEFT JOIN event_reservation_users ON event_reservations.event_id = UUID_TO_BIN(?) AND event_reservations.id = event_reservation_users.reservation_id GROUP BY event_reservations.id", userId, id)
+	rows, err := c.DB.Query("SELECT BIN_TO_UUID(event_reservations.id), event_reservations.name, event_reservations.Description,start_date, finish_date, reservation_start_date, reservation_finish_date, event_reservations.capacity, event_reservations.capacity - count(event_reservation_users.id), IF(SUM(event_reservation_users.user_id = UUID_TO_BIN(?)) ,true ,false) AS reservated  FROM event_reservations LEFT JOIN event_reservation_users ON event_reservations.event_id = UUID_TO_BIN(?) AND event_reservations.id = event_reservation_users.reservation_id GROUP BY event_reservations.id", userId, id)
 	if err != nil {
-		return e.JSON(http.StatusInternalServerError, ResponseEventsList{Error: "DBの読み込みに失敗しました"})
+		return e.JSON(http.StatusInternalServerError, ResponseEventsList{Error: err.Error()})
 	}
 	defer rows.Close()
-	details := []Detail{}
+	reservation_frames := []ReservationFrame{}
 	for rows.Next() {
-		detail := Detail{}
-		if err := rows.Scan(&detail.Id, &detail.Name, &detail.Description, &detail.Full, &detail.Reservated); err != nil {
+		reservation_frame := ReservationFrame{}
+		if err := rows.Scan(&reservation_frame.Id, &reservation_frame.Name, &reservation_frame.Description, &reservation_frame.StartDate, &reservation_frame.FinishDate, &reservation_frame.ReservationStartDate, &reservation_frame.ReservationFinishDate, &reservation_frame.Capacity, &reservation_frame.FreeCapacity, &reservation_frame.Reservated); err != nil {
 			return e.JSON(http.StatusInternalServerError, ResponseEventsList{Error: "DBの読み込みに失敗しました"})
 		}
-		details = append(details, detail)
+		now_date := time.Now()
+		if reservation_frame.FreeCapacity == 0 || now_date.Before(reservation_frame.ReservationStartDate) || now_date.After(reservation_frame.ReservationFinishDate) {
+			reservation_frame.Reservable = false
+		} else {
+			reservation_frame.Reservable = true
+		}
+		reservation_frames = append(reservation_frames, reservation_frame)
 	}
-	response := ResponseEventDetail{Detail: details}
+	response := ResponseEventDetail{ReservationFrame: reservation_frames}
 	err = c.DB.QueryRow("SELECT BIN_TO_UUID(events.id), events.name, events.description, (CASE WHEN user_id IS NOT NULL THEN true ELSE false END) AS reservated FROM events LEFT JOIN event_reservations ON events.id = event_reservations.event_id LEFT JOIN event_reservation_users ON event_reservations.id = event_reservation_users.reservation_id AND event_reservation_users.user_id = UUID_TO_BIN(?) WHERE events.id = UUID_TO_BIN(?)", userId, id).Scan(&response.Id, &response.Name, &response.Description, &response.Reservated)
 	if err != nil {
 		return e.JSON(http.StatusInternalServerError, ResponseEventsList{Error: "DBの読み込みに失敗しました"})
 	}
-	return e.JSON(http.StatusOK,response )
+	return e.JSON(http.StatusOK, response)
 }
