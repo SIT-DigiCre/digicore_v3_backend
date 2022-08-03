6ae62f7f12ba19dd6190f3130c216e9e9d930ccf
ecto0310 <ecto0310@gmail.com>
2022-08-03 12:30:03 +0900

    [add] other user profile

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -65,6 +65,8 @@ func addRouting(e *echo.Echo, db *sql.DB) {
 	user_group.PUT("/my/payment", user.UpdateMyPayment)
 	user_group.GET("/my/payment", user.GetMyPayment)
 	user_group.GET("/my/payment/history", user.GetMyPaymentHistory)
+	user_group.GET("/:id", user.GetMyProfile)
+	user_group.GET("/:id/introduction", user.GetSelfIntroduction)
 
 	event_group := e.Group("/event")
 	event_group.Use(middleware.JWTWithConfig(config))
