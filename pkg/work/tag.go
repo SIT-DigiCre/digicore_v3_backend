5f3648569c6f09a793d44bb69aaea45347dfd626
ecto0310 <ecto0310@gmail.com>
2022-09-16 14:51:36 +0900

    [add] CRUD tag

--- pkg/work/tag.go
+++ pkg/work/tag.go
@@ -1,21 +1,24 @@
 package work
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
-type RequestCreateTag struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-}
+type RequestCreateTag = UpdatgeTag
 
 type ResponseCreateTag struct {
-	Success bool `json:"success"`
+	Success bool   `json:"success"`
+	ID      string `json:"id"`
 }
 
-type RequestUpdatgeTag struct {
+type RequestUpdatgeTag = UpdatgeTag
+
+type UpdatgeTag struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
@@ -40,7 +43,28 @@ type ResponseDeleteTag struct {
 // @Param RequestCreateTag body RequestCreateTag true "update work"
 // @Success 200 {object} ResponseCreateTag
 func (c Context) CreateTag(e echo.Context) error {
-	return e.JSON(http.StatusOK, ResponseCreateTag{})
+	tag := UpdatgeTag{}
+	if err := e.Bind(&tag); err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "データの読み込みに失敗しました"})
+	}
+	randomName, err := uuid.NewRandom()
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "タグの追加に失敗しました"})
+	}
+	_, err = c.DB.Exec("INSERT INTO work_tags (name, description) VALUES (?, ?)", randomName.String(), "")
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "タグの追加に失敗しました"})
+	}
+	id := ""
+	err = c.DB.QueryRow("SELECT BIN_TO_UUID(id) FROM work_tags WHERE name = ?", randomName.String()).Scan(&id)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "タグの追加に失敗しました"})
+	}
+	err = updateTag(c.DB, id, tag)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "タグの追加に失敗しました"})
+	}
+	return e.JSON(http.StatusOK, ResponseCreateTag{Success: true, ID: id})
 }
 
 // Update tag
@@ -51,7 +75,16 @@ func (c Context) CreateTag(e echo.Context) error {
 // @Param RequestUpdatgeTag body RequestUpdatgeTag true "update work"
 // @Success 200 {object} ResponseUpdatgeTag
 func (c Context) UpdateTag(e echo.Context) error {
-	return e.JSON(http.StatusOK, ResponseUpdatgeTag{})
+	tag := UpdatgeTag{}
+	if err := e.Bind(&tag); err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "データの読み込みに失敗しました"})
+	}
+	id := e.Param("id")
+	err := updateTag(c.DB, id, tag)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "タグの追加に失敗しました"})
+	}
+	return e.JSON(http.StatusOK, ResponseUpdatgeTag{Success: true})
 }
 
 // Get tag
@@ -61,7 +94,13 @@ func (c Context) UpdateTag(e echo.Context) error {
 // @Param id path string true "tag id"
 // @Success 200 {object} ResponseGetTag
 func (c Context) GetTag(e echo.Context) error {
-	return e.JSON(http.StatusOK, ResponseGetTag{})
+	id := e.Param("id")
+	tag := ResponseGetTag{}
+	err := c.DB.QueryRow("SELECT name, description FROM work_tags WHERE id = UUID_TO_BIN(?)", id).Scan(&tag.Name, &tag.Description)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: err.Error()})
+	}
+	return e.JSON(http.StatusOK, tag)
 }
 
 // Get tag
@@ -71,5 +110,18 @@ func (c Context) GetTag(e echo.Context) error {
 // @Param id path string true "tag id"
 // @Success 200 {object} ResponseDeleteTag
 func (c Context) DeleteTag(e echo.Context) error {
-	return e.JSON(http.StatusOK, ResponseDeleteTag{})
+	id := e.Param("id")
+	_, err := c.DB.Exec("DELETE FROM work_tags WHERE id = UUID_TO_BIN(?)", id)
+	if err != nil {
+		return e.JSON(http.StatusBadRequest, Error{Message: "作品の削除に失敗しました"})
+	}
+	return e.JSON(http.StatusOK, ResponseDeleteTag{Success: true})
+}
+
+func updateTag(db *sql.DB, id string, tag UpdatgeTag) error {
+	_, err := db.Exec("UPDATE work_tags SET name = ?, description = ? WHERE id = UUID_TO_BIN(?)", tag.Name, tag.Description, id)
+	if err != nil {
+		return fmt.Errorf("タグの編集に失敗しました")
+	}
+	return nil
 }
