fc959d58dea3bac1484fa501dc9f2c5637023598
ecto0310 <ecto0310@gmail.com>
2022-10-07 22:02:25 +0900

    [fix] query

--- pkg/db/sql/users/select_user_payment_from_user_id.sql
+++ pkg/db/sql/users/select_user_payment_from_user_id.sql
@@ -1,1 +1,1 @@
-SELECT year, transfer_name, checked, created_at FROM user_payments WHERE user_id = UUID_TO_BIN(/*userID*/'aa21000');
+SELECT year, transfer_name, checked, created_at FROM user_payments WHERE user_id = UUID_TO_BIN(/*userID*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee');
