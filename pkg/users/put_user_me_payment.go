eb215a84761edb30b04c82e1cf12ad09ee4cce51
ecto0310 <ecto0310@gmail.com>
2022-10-09 00:44:16 +0900

    [add] put user payment

--- pkg/users/put_user_me_payment.go
+++ pkg/users/put_user_me_payment.go
@@ -6,35 +6,31 @@ import (
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/api"
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/api/response"
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/db"
+	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/utils"
 	"github.com/labstack/echo/v4"
 )
 
-func PutUserMePayment(ctx echo.Context, dbClient db.TransactionClient, requestBody api.ReqPutUserMePayment) (api.ResGetUserMePayment, *response.Error) {
+func PutUserMePayment(ctx echo.Context, dbClient db.TransactionClient, requestBody api.ReqPutUserMePayment) ([]api.ResGetUserMePayment, *response.Error) {
 	userID := ctx.Get("user_id").(string)
 	err := updateUserPayment(dbClient, userID, requestBody)
 	if err != nil {
-		return api.ResGetUserMePayment{}, err
+		return []api.ResGetUserMePayment{}, err
 	}
 
-	_, err = getUserProfileFromUserID(userID, dbClient)
-	if err != nil {
-		return api.ResGetUserMePayment{}, err
-	}
-	res := api.ResGetUserMePayment{}
-	return res, nil
+	return GetUserMePayment(ctx, dbClient)
 }
 
 func updateUserPayment(dbClient db.TransactionClient, userID string, requestBody api.ReqPutUserMePayment) *response.Error {
 	params := struct {
-		UserID                string `twowaysql:"userID"`
-		IconUrl               string `twowaysql:"iconURL"`
-		SchoolGrade           int    `twowaysql:"schoolGrade"`
-		ShortSelfIntroduction string `twowaysql:"shortSelfIntroduction"`
-		Username              string `twowaysql:"username"`
+		UserID       string `twowaysql:"userID"`
+		Year         int    `twowaysql:"year"`
+		TransferName string `twowaysql:"transferName"`
 	}{
-		UserID: userID,
+		UserID:       userID,
+		Year:         utils.GetSchoolYear(),
+		TransferName: requestBody.TransferName,
 	}
-	_, err := dbClient.DuplicateUpdate("sql/users/insert_user_profile.sql", "sql/users/update_user_profile.sql", &params)
+	_, err := dbClient.DuplicateUpdate("sql/users/insert_user_payment.sql", "sql/users/update_user_payment.sql", &params)
 	if err != nil {
 		return &response.Error{Code: http.StatusInternalServerError, Level: "Info", Message: "DBエラーが発生しました", Log: err.Error()}
 	}
