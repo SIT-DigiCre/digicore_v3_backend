f86e0f2fb824d8fc4ed5e951906c7b72a8b1ce4c
ecto0310 <ecto0310@gmail.com>
2022-11-13 01:33:54 +0900

    [add] get strage

--- pkg/api/server/get_storage.go
+++ pkg/api/server/get_storage.go
@@ -2,12 +2,15 @@ package server
 
 import (
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/api/response"
+	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/db"
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/storage"
 	"github.com/labstack/echo/v4"
 )
 
 func (s *server) GetStorage(ctx echo.Context) error {
-	res, err := storage.GetStorage(ctx)
+	dbClient := db.Open()
+
+	res, err := storage.GetStorage(ctx, &dbClient)
 	if err != nil {
 		return response.ErrorResponse(ctx, err)
 	}
