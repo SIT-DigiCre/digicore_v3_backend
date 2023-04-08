b04a10dab690c0976fa5e371ad435f57f8b1ad20
ecto0310 <ecto0310@gmail.com>
2023-04-08 23:20:04 +0900

    [fix] query

--- pkg/db/sql/user/update_user_payment.sql
+++ pkg/db/sql/user/update_user_payment.sql
@@ -1,1 +1,1 @@
-UPDATE user_payments SET transfer_name = /*transferName*/'name' WHERE user_id = UUID_TO_BIN(/*userId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee') AND checked = false AND year = /*year*/2022,;
+UPDATE user_payments SET transfer_name = /*transferName*/'name' WHERE user_id = UUID_TO_BIN(/*userId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee') AND checked = false AND year = /*year*/2022;
