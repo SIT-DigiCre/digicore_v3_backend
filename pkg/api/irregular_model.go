aa8bf8e7f3293e823954e305500b064b6c8435fd
ecto0310 <me_github@ecto0310.jp>
2023-06-14 21:54:09 +0900

    [fix] fix mattermost remind

--- pkg/api/irregular_model.go
+++ pkg/api/irregular_model.go
@@ -3,7 +3,7 @@ package api
 type ReqPostMattermostCmd struct {
 	ChannelName string `form:"channel_name" ja:"チャンネル名" validate:"required"`
 	Command     string `form:"command" ja:"コマンド" validate:"required"`
-	Text        string `form:"text" ja:"テキスト" validate:"required"`
+	Text        string `form:"text" ja:"テキスト"`
 	Token       string `form:"token" ja:"トークン" validate:"required"`
 	UserName    string `form:"user_name" ja:"ユーザー名" validate:"required"`
 }
