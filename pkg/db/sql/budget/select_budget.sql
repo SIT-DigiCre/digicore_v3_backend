e1775c5687a6cb346f327d62cf8c85bf97a95134
saka-naname <reisuicupsoup@gmail.com>
2024-03-16 03:41:51 +0900

    [fix] column 'updated_at' in field list is ambiguous

--- pkg/db/sql/budget/select_budget.sql
+++ pkg/db/sql/budget/select_budget.sql
@@ -1,1 +1,1 @@
-SELECT BIN_TO_UUID(`budgets`.id) as budget_id, BIN_TO_UUID(proposer_user_id) as user_id, username as user_name, icon_url, `status`, class, name, budget, settlement, updated_at FROM `budgets` LEFT JOIN `user_profiles` ON `budgets`.proposer_user_id = `user_profiles`.user_id;
+SELECT BIN_TO_UUID(`budgets`.id) as budget_id, BIN_TO_UUID(proposer_user_id) as user_id, username as user_name, icon_url, `status`, class, name, budget, settlement, `budgets`.updated_at as updated_at FROM `budgets` LEFT JOIN `user_profiles` ON `budgets`.proposer_user_id = `user_profiles`.user_id;
