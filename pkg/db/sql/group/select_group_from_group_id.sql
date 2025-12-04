034592ec5750e2c85a66dfe8b6de21d8f08ef341
newt <ysti2019.win2020.st@gmail.com>
2025-12-04 16:57:17 +0900

    feat: add isAdminGroup field

--- pkg/db/sql/group/select_group_from_group_id.sql
+++ pkg/db/sql/group/select_group_from_group_id.sql
@@ -1,1 +1,13 @@
-SELECT BIN_TO_UUID(groups.id) as group_id, name, description, user_count, joinable, IF(1 <= count(user_id), true, false) as joined FROM `groups` LEFT JOIN groups_users ON groups_users.group_id = groups.id AND user_id = UUID_TO_BIN(/*userId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee') WHERE groups.id = UUID_TO_BIN(/*groupId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee') GROUP BY groups.id;
+SELECT 
+  BIN_TO_UUID(groups.id) as group_id,
+  name,
+  description,
+  user_count,
+  joinable,
+  IF(1 <= count(user_id), true, false) as joined,
+  IF(COUNT(group_claims.group_id) > 0, true, false) as is_admin_group
+FROM `groups`
+LEFT JOIN groups_users ON groups_users.group_id = groups.id AND user_id = UUID_TO_BIN(/*userId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee')
+LEFT JOIN group_claims ON group_claims.group_id = groups.id AND group_claims.claim = 'admin'
+WHERE groups.id = UUID_TO_BIN(/*groupId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee')
+GROUP BY groups.id;
