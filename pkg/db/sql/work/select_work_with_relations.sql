a6cd1570025be9c998d14ff4ac8ca87aa11cb101
newt <ysti2019.win2020.st@gmail.com>
2026-01-25 17:25:49 +0900

    fix: sql query

--- pkg/db/sql/work/select_work_with_relations.sql
+++ pkg/db/sql/work/select_work_with_relations.sql
@@ -6,17 +6,21 @@ SELECT
     user_profiles.icon_url AS author_icon_url,
     BIN_TO_UUID(work_work_tags.tag_id) AS tag_id,
     work_tags.name AS tag_name
-FROM works
-/* IF authorId */
-INNER JOIN work_users AS filter_users ON filter_users.work_id = works.id 
-    AND filter_users.user_id = UUID_TO_BIN(/*authorId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee')
-/* END */
+FROM (
+    SELECT works.id, works.name, works.updated_at
+    FROM works
+    /* IF authorId */
+    INNER JOIN work_users AS filter_users ON filter_users.work_id = works.id 
+        AND filter_users.user_id = UUID_TO_BIN(/*authorId*/'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee')
+    /* END */
+    ORDER BY works.updated_at DESC
+    LIMIT 10
+    /* IF offset */
+    OFFSET /*offset*/0
+    /* END */
+) AS works
 LEFT JOIN work_users ON work_users.work_id = works.id
 LEFT JOIN user_profiles ON user_profiles.user_id = work_users.user_id
 LEFT JOIN work_work_tags ON work_work_tags.work_id = works.id
 LEFT JOIN work_tags ON work_tags.id = work_work_tags.tag_id
-ORDER BY works.updated_at DESC
-LIMIT 10
-/* IF offset */
-OFFSET /*offset*/0
-/* END */;
+ORDER BY works.updated_at DESC;
