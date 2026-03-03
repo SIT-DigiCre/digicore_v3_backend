6d5cdfb1fb2d52c1bf75103252bc4b120724028e
newt <ysti2019.win2020.st@gmail.com>
2026-03-03 23:49:56 +0900

    feat: 管理者向け学籍番号変更APIを追加

---
+++ pkg/admin/put_admin_change_student_number.go
@@ -0,0 +1,59 @@
+package admin
+
+import (
+	"net/http"
+
+	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/api"
+	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/api/response"
+	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/db"
+	"github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
+)
+
+func PutAdminChangeStudentNumber(_ echo.Context, dbClient db.TransactionClient, requestBody api.ReqPutAdminChangeStudentNumber) (api.BlankSuccess, *response.Error) {
+	params := struct {
+		UserId        string `twowaysql:"userId"`
+		StudentNumber string `twowaysql:"studentNumber"`
+	}{
+		UserId:        requestBody.UserId,
+		StudentNumber: requestBody.StudentNumber,
+	}
+
+	result, err := dbClient.Exec("sql/admin/update_user_student_number.sql", &params, false)
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == 1062 {
+			return api.BlankSuccess{}, &response.Error{
+				Code:    http.StatusBadRequest,
+				Level:   "Info",
+				Message: "指定された学籍番号は既に使用されています",
+				Log:     err.Error(),
+			}
+		}
+		return api.BlankSuccess{}, &response.Error{
+			Code:    http.StatusInternalServerError,
+			Level:   "Error",
+			Message: "不明なエラーが発生しました",
+			Log:     err.Error(),
+		}
+	}
+
+	rowsAffected, rerr := result.RowsAffected()
+	if rerr != nil {
+		return api.BlankSuccess{}, &response.Error{
+			Code:    http.StatusInternalServerError,
+			Level:   "Error",
+			Message: "不明なエラーが発生しました",
+			Log:     rerr.Error(),
+		}
+	}
+	if rowsAffected == 0 {
+		return api.BlankSuccess{}, &response.Error{
+			Code:    http.StatusBadRequest,
+			Level:   "Info",
+			Message: "更新対象のユーザーが存在しません",
+			Log:     "user not found",
+		}
+	}
+
+	return api.BlankSuccess{Success: true}, nil
+}
