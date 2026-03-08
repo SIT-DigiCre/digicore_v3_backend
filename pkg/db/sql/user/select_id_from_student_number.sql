855a667beb4bb2ad12b39a02c0c7cd565367168d
newt <ysti2019.win2020.st@gmail.com>
2026-03-08 23:09:37 +0900

    fix: 学籍番号検索SQLの返却カラム不一致を解消

--- pkg/db/sql/user/select_id_from_student_number.sql
+++ pkg/db/sql/user/select_id_from_student_number.sql
@@ -1,6 +1,5 @@
 SELECT
-    BIN_TO_UUID(users.id) AS id,
-    COALESCE(user_profiles.is_member, false) AS is_member
+    BIN_TO_UUID(users.id) AS id
 FROM users
 LEFT JOIN user_profiles ON users.id = user_profiles.user_id
 WHERE student_number = /*studentNumber*/'aa21000';
