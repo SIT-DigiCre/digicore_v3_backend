2046a532276b783e0755ad85a348811e7b43c6db
Moritomo174 <250947871+Moritomo174@users.noreply.github.com>
2026-05-04 09:17:10 +0900

    fix/ copierを使う形から直に代入する形に変更

--- pkg/user/get_user_user_id.go
+++ pkg/user/get_user_user_id.go
@@ -2,9 +2,10 @@ package user
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/jinzhu/copier"
-
+    
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/api"
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/api/response"
 	"github.com/SIT-DigiCre/digicore_v3_backend/pkg/db"
@@ -22,14 +23,31 @@ func GetUserUserId(ctx echo.Context, dbClient db.Client, userId string) (api.Res
 		return api.ResGetUserUserId{}, &response.Error{Code: http.StatusInternalServerError, Level: "Error", Message: "不明なエラーが発生しました", Log: rerr.Error()}
 	}
 
-	linkUrls, err := GetUserProfileLinksFromUserId(dbClient, userId)
-	if err != nil {
-		return api.ResGetUserUserId{}, err
-	}
-	rerr = copier.Copy(&res.LinkUrls, &linkUrls)
-	if rerr != nil {
-		return api.ResGetUserUserId{}, &response.Error{Code: http.StatusInternalServerError, Level: "Error", Message: "不明なエラーが発生しました", Log: rerr.Error()}
-	}
+    links, err := GetUserProfileLinksFromUserId(dbClient, userId)
+    if err != nil {
+        return api.ResGetUserUserId{}, err
+    }
 
-	return res, nil
+    res.LinkUrls = []struct {
+        CreatedAt time.Time `json:"createdAt"`
+        Id        string    `json:"id"`
+        LinkUrl   string    `json:"linkUrl"`
+        UpdatedAt time.Time `json:"updatedAt"`
+    }{}
+
+    for _, l := range links {
+        res.LinkUrls = append(res.LinkUrls, struct {
+            CreatedAt time.Time `json:"createdAt"`
+            Id        string    `json:"id"`
+            LinkUrl   string    `json:"linkUrl"`
+            UpdatedAt time.Time `json:"updatedAt"`
+        }{
+            CreatedAt: l.CreatedAt,
+            Id:        l.Id,
+            LinkUrl:   l.LinkUrl,
+            UpdatedAt: l.UpdatedAt,
+        })
+    }
+
+    return res, nil
 }
