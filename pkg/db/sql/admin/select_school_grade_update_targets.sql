c31a971ff8e8f177b70c010ba7e264f73817a72a
Haruhiko FUJI <contact@wisteriatp.work>
2026-05-16 02:23:19 +0900

    fix: 大学院生の学年計算ロジックを修正 (#342)
    * fix: 学生番号からの学年計算ロジックを修正
    
    * fix: テストコードを更新
    
    * SQLで大学院生が学年更新対象から外れていた問題を修正

--- pkg/db/sql/admin/select_school_grade_update_targets.sql
+++ pkg/db/sql/admin/select_school_grade_update_targets.sql
@@ -15,17 +15,9 @@ LEFT JOIN (
 WHERE user_profiles.is_graduated = false
   AND (
       user_profiles.is_member = true
-      OR user_profiles.school_grade < 6
+      OR user_profiles.school_grade < 4
   )
   AND (
-      (
-          LOWER(LEFT(users.student_number, 1)) IN ('m', 'n')
-          AND CHAR_LENGTH(users.student_number) >= 4
-          AND SUBSTRING(users.student_number, 2, 2) REGEXP '^[0-9]{2}$'
-      )
-      OR (
-          LOWER(LEFT(users.student_number, 1)) NOT IN ('m', 'n')
-          AND CHAR_LENGTH(users.student_number) >= 4
-          AND SUBSTRING(users.student_number, 3, 2) REGEXP '^[0-9]{2}$'
-      )
+      CHAR_LENGTH(users.student_number) >= 4
+      AND SUBSTRING(users.student_number, 3, 2) REGEXP '^[0-9]{2}$'
   );
