666c2c4c6f13184e1bd39aa63261e1534a01dd4f
Moritomo174 <250947871+Moritomo174@users.noreply.github.com>
2026-08-25 21:10:21 +0900

    feat: delete_user_private_profiles

--- pkg/db/sql/admin/update_user_profiles_inactive.sql
+++ pkg/db/sql/admin/update_user_profiles_inactive.sql
@@ -1,5 +1,22 @@
 UPDATE user_profiles
-SET is_member = false
-WHERE active_limit < CURRENT_DATE
-  AND is_member = true
-  AND is_graduated = false;
+LEFT JOIN user_private_profiles
+  ON user_private_profiles.user_id = user_profiles.user_id
+SET user_profiles.is_member = false,
+    user_private_profiles.first_name = '',
+    user_private_profiles.last_name = '',
+    user_private_profiles.first_name_kana = '',
+    user_private_profiles.last_name_kana = '',
+    user_private_profiles.is_male = false,
+    user_private_profiles.phone_number = '',
+    user_private_profiles.address = '',
+    user_private_profiles.parent_name = '',
+    user_private_profiles.parent_last_name = '',
+    user_private_profiles.parent_first_name = '',
+    user_private_profiles.parent_cellphone_number = '',
+    user_private_profiles.parent_homephone_number = '',
+    user_private_profiles.parent_address = ''
+WHERE (user_profiles.active_limit < CURRENT_DATE
+       AND user_profiles.is_member = true
+       AND user_profiles.is_graduated = false)
+   OR user_profiles.is_member = false;
+   /* is_member = falseも条件に入れることでこれまでの先輩方の個人情報も消えるよう調整
